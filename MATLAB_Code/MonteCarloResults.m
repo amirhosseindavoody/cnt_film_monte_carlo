b34e03477826f17ca2258fe03b572fc69290991e
gbear47 <agabourie@wisc.edu>
2015-07-22 23:38:28 -0500

    have importing code that works now

--- MATLAB_Code/MonteCarloResults.m
+++ MATLAB_Code/MonteCarloResults.m
@@ -1,5 +1,21 @@
 %% Monte Carlo Results
 % Imports the results from file that the Monte Carlo simulation output.
 
-[thetas,rs,hm] = importRThetaDist();
-[numExcitons,Tmax,deltaT,numRegions,xdim,t,dist] = importExcitonDist();
+folder = uigetdir(pwd);
+
+if(exist([folder '.mat'],'file') ~= 2) 
+
+    [numExcitons,Tmax,deltaT,numRegions,xdim,minBin,rmax,numBins,lowAng,highAng,numAng]...
+        = importDetails([folder '/details.csv']);
+
+    heatMap = importRThetaDist([folder '/heatMap.csv']);
+
+    [t, excitonDist] = importExcitonDist([folder '/excitonDist.csv']);
+
+    save([folder '.mat']);
+
+else
+    
+    load([folder '.mat']);
+    
+end
