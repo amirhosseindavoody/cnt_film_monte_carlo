794600fb851594243cdeba384b6a5d6af18fd6bd
DangerNoodle <agabourie@wisc.edu>
2015-08-14 11:51:58 -0500

    enum and member variables in etrans header

--- CNTMC_Project/CNTMC_Project/energyTransition.h
+++ CNTMC_Project/CNTMC_Project/energyTransition.h
@@ -1,6 +1,23 @@
 #pragma once
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+//Type to store energy information
+enum energy
+{
+	E11,
+	E22
+};
+
 class energyTransition
 {
+
+	energy source_energy; //energy of exciton on source segment
+	energy dest_energy; //energy of exciton on destination segment
+	shared_ptr<vector<shared_ptr<vector<double>>>> rtTable; // r and theta table values
+
 public:
 	energyTransition();
 	~energyTransition();
