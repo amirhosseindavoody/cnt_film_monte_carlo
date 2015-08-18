fe84ca1077427e7a757c48a4d6b5619f1d026363
gbear47 <agabourie@wisc.edu>
2015-08-18 00:30:33 -0500

    constructor for type transition fixed

--- CNTMC_Project/CNTMC_Project/typeTransition.cpp
+++ CNTMC_Project/CNTMC_Project/typeTransition.cpp
@@ -25,7 +25,11 @@ typeTransition::typeTransition(Chirality &src, Chirality &dest, uint32_t r_lengt
 	dest_chirality = dest;
 	ETransList = make_shared<vector<energyTransition>>(vector<energyTransition>(0));
 	//initialize array
-	std::array<
+	array<energy, 2> elist{ { E11, E22 } };
+	for (uint8_t i = 0x00; i < 0x04; i++)
+	{
+		ETransList->push_back(energyTransition(elist[(0x02 & i) >> 1], elist[0x01 & i], r_length, theta_length));
+	}
 
 }
 
