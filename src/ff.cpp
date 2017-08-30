bfab51180946a31fce83cc3aad01a425ba9b6522
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2017-08-30 11:30:15 -0500

    some code added for boundary checking
    further inspection might be needed

--- src/ff.cpp
+++ src/ff.cpp
@@ -16,11 +16,12 @@ void free_flight::fly(mc::arr1d &pos, mc::arr1d &mom, const mc::t_float &eff_mas
 {
 	const mc::t_float coeff_1 = dt/eff_mass;
 	const mc::t_float coeff_2 = dt*dt/2.;
+	const mc::t_float coeff_3 = eff_mass*dt;
 
 	for (int i=0; i<pos.size(); ++i)
 	{
 		pos[i] = pos[i] + mom[i]*coeff_1 + _acceleration[i]*coeff_2;
-		mom[i] = mom[i] + _acceleration[i]*dt;
+		mom[i] = mom[i] + _acceleration[i]*coeff_3;
 	}
 };
 
@@ -37,11 +38,19 @@ void free_flight::check_boundary(mc::arr1d &pos, mc::arr1d &mom, const mc::arr1d
 	const mc::t_float coeff_1 = dt/eff_mass;
 	const mc::t_float coeff_2 = dt*dt/2.;
 
+	mc::arr1d old_velocity;
+	for (int i=0; i<mom.size(); ++i)
+	{
+		old_velocity[i] = old_mom[i]/eff_mass;
+	}
+
 	for (int i=0; i<pos.size(); ++i)
 	{
 		if (pos[i] > volume[i])
 		{
-			t0 = 
+			t0 = (-old_velocity[i]+std::sqrt(std::pow(old_velocity[i],2)-2.*_acceleration[i]*(old_pos[i]-volume[i])))/(_acceleration[i]);
+			pos[i] = old_pos[i] + (-old_velocity[i]-_acceleration[i]*t0)*(dt-t0) + _acceleration[i]*std::pow(dt-t0,2)/2.;
+			mom[i] = eff_mass*((-old_velocity[i]-_acceleration[i]*t0)+_acceleration[i]*(dt-t0));
 		}
 		else if(pos[i] < 0)
 		{
