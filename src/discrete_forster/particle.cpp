d652a61d2829d0fdd2524cb83b31ab2ae40a7a63
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2018-06-21 17:03:35 -0500

    debugging trim scatterer list

--- src/discrete_forster/particle.cpp
+++ src/discrete_forster/particle.cpp
@@ -7,7 +7,7 @@ namespace mc
 {
   // perform free flight within the simulation domain
   void particle::fly(double dt, const std::vector<scatterer>& s_list) {
-    
+
     int next;
 
     while (true){
@@ -33,15 +33,23 @@ namespace mc
       else
         _heading_right = false;
       
-      arma::vec t = s_list[next].pos();
+      // arma::vec t = s_list[next].pos();
+      if (next<0){
+        std::cout << "next is less than zero!!!" << std::endl;
+      }
+
+      arma::vec t = s_list.at(next).pos();
       double dist = arma::norm(_pos - t);
 
       if (dist / _velocity < dt) {
-        _pos = s_list[next].pos();
-        _scat_ptr = &(s_list[next]);
+        // _pos = s_list[next].pos();
+        // _scat_ptr = &(s_list[next]);
+        _pos = s_list.at(next).pos();
+        _scat_ptr = &(s_list.at(next));
         dt -= dist / _velocity;
       } else {
-        arma::vec d = arma::normalise(s_list[next].pos() - _pos);
+        // arma::vec d = arma::normalise(s_list[next].pos() - _pos);
+        arma::vec d = arma::normalise(s_list.at(next).pos() - _pos);
         _pos += (_velocity * dt * d);
         return;
       }
